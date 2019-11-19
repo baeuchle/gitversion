418c32ac27bac77355d2fb6868946203ce9fe78f
Bjørn Bäuchle <baeuchle@frankfurtium.de>
2019-11-19 13:58:07 +0100

    CHG OOP, several new functions

--- __init__.py
+++ __init__.py
@@ -5,16 +5,46 @@ from subprocess import Popen, PIPE
 
 """Module for simple git information tasks"""
 
-def describe():
-    """
-        Return a description of the current git version of one directory up
-    """
-    result = ""
-    with Popen(['git', 'describe', '--always'],
-        stdout=PIPE,
-        cwd=Path(__file__).parents[1],
-        universal_newlines=True
-        ) as proc:
-        for line in proc.stdout:
+class Git:
+    def __init__(self, cwd=Path(__file__).parents[1]):
+        self.cwd = cwd
+        self.popen_kwargs = {
+            'stdout': PIPE,
+            'cwd': self.cwd,
+            'universal_newlines': True
+            }
+
+    def _gitlines(self, *git_args):
+        result = []
+        cmd_args = ['git']
+        for arg in git_args:
+            cmd_args.append(arg)
+        with Popen(cmd_args, **self.popen_kwargs) as proc:
+            for line in proc.stdout:
+                result.append(line)
+        return result
+
+    def _gitoutput(self, *git_args):
+        result = ""
+        for line in self._gitlines(*git_args):
             result += line
-    return result.strip()
+        return result.strip()
+
+    def describe(self):
+        return self._gitoutput('describe', '--always')
+
+    def hash(self, *args):
+        return self._gitoutput('log', '--pretty=%H', '-1', *args)
+
+    def changelog(self, old_version=None):
+        result = ""
+        if old_version == None or old_version.strip() == '':
+            old_version = self._gitlines('log', '--pretty=%H')[-1].strip()
+        for line in self._gitlines('log', '{}..HEAD'.format(old_version)):
+            trimmed = line.strip()
+            if len(trimmed) <= len('CHANGELOG'):
+                continue
+            if trimmed[0:9] != 'CHANGELOG':
+                continue
+            result += '•' + trimmed[9:] + '\n'
+        return result
